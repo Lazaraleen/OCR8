b966af1874029948b66ae39c3638204c287a4cfa
Myriam JAYLE <myriamjayle@gmail.com>
2023-05-04 18:23:02 +0200

    petit pas

--- src/components/MainHome.jsx
+++ src/components/MainHome.jsx
@@ -1,13 +1,30 @@
 import falaise from '../assets/images/Falaise.png';
 import '../style/MainHome.css';
 
+function MiseEnAvant() {
+    return (
+        <div className='miseenavant'>
+            <img src={falaise} className="falaise" alt="falaise" />   
+            <h1>Chez vous, partout et ailleurs</h1>         
+        </div>
+    )
+}
+
+function Location() {
+    return (
+        <div className='locations'>
+            <div className="locate">
+                <p>Titre de la location</p>
+            </div>
+        </div>
+    )
+}
+
 function MainHome() {
     return (
         <div className='mainhome'>
-            <div className='miseenavant'>
-                <img src={falaise} className="falaise" alt="falaise" />   
-                <h1>Chez vous, partout et ailleurs</h1>         
-            </div>
+            <MiseEnAvant />
+            <Location />
         </div>
     )
 }
